4c8c5219497006650b061dd8d3d23ac07aa11223
Muhammad Raihan Rahmadian <141240073+code-ying@users.noreply.github.com>
2024-02-10 23:09:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import NavbarComponent from "./components/NavbarComponent";
 import FooterComponent from "./components/FooterComponent";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 import KelasPage from "./pages/KelasPage";
 import TestimonialPage from "./pages/TestimonialPage";
 import FaqPage from "./pages/FaqPage";
